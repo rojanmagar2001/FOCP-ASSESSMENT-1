da05e87fdf422271ca8cea27b4fe6a0261cb6b19
rojanmagar2001 <87289875+rojanmagar2001@users.noreply.github.com>
2022-01-08 12:30:02 +0545

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,6 +1,5 @@
 from os import name
 import sys
-import pandas as pd
 
 class Team: # Class which store all the information of the team
     def __init__(self):
@@ -39,7 +38,6 @@ class Team: # Class which store all the information of the team
         return [self.name, self.played, self.wins, self.draw, self.loss, self.score, self.conceded, self.goal_diff, self.points]
 
 
-
 # Four Function is made as there are 4 teams
 t1 = Team()
 t2 = Team()
@@ -47,41 +45,42 @@ t3 = Team()
 t4 = Team()
 
 team_list  = list((t1, t2, t3, t4)) # Storing the team object in the list
-t = pd.read_csv("C:\\Users\Surunga Education\Documents\Python_Class\Assessment\Assessment\FOCP ASSESSMENT\Team.csv")
+t = open('C:\\Users\Surunga Education\Documents\Python_Class\Assessment\Team.csv')
 
-for index,row in t.iterrows(): # Getting the name of the team and storing in the team objects
+for lines in t: # Getting the name of the team and storing in the team objects
+    lines=lines.rstrip("\n")
     for i in team_list:
         if i.getTeam_name() == "":
-            i.setTeam_name(row['Team'])
+            i.setTeam_name(lines)
             break
+t.close()
 
+result = open("C:\\Users\Surunga Education\Documents\Python_Class\Assessment\Result.csv") # Reading the games result
 
-result = pd.read_csv("C:\\Users\Surunga Education\Documents\Python_Class\Assessment\Assessment\FOCP ASSESSMENT\Result.csv") # Reading the games result
-
-for index,col in result.iterrows(): # Updating the stats one by one 
+for lines in result: # Updating the stats one by one
+    game = lines.split(",")
     for i in team_list:
-        if col['Home'] == i.getTeam_name():
-            i.update(col['Home_Goal'], col['Away_Goal'])
+        if game[0] == i.getTeam_name():
+            i.update(int(game[1]), int(game[3]))
             break
 
     for i in team_list:
-        if col['Away'] == i.getTeam_name():
-            i.update(col['Away_Goal'], col['Home_Goal'])
+        if game[2] == i.getTeam_name():
+            i.update(int(game[3]), int(game[1]))
             break
+result.close()
     
-result = [] # List for storing the teams result
+result = []
 for i in team_list: 
     result.append(i.getStat())
 
 # Sorting the result by the pts, g.d. and score
 result.sort(key = lambda row : (row[8], row[7], row[5]), reverse=True)
 
-# Checking if command line argument is given or not, if given printing it
 if len(sys.argv) != 1:
     print(sys.argv[1])
     print(len(sys.argv[1])*"=")
 
-# Printing out league table using f-string formatting
 print(f"{'Name':<9} {'P':<2} {'W':<2} {'D':<2} {'L':<2} {'S':<3} {'C':<3} {'G.D.':<4} {'Pts.':<4}")
 
 for i in result:
